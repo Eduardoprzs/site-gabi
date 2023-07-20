0bfd9276899d2c561d3ca38982792e28217a3ce8
Eduardo Prazeres <104949104+Eduardoprzs@users.noreply.github.com>
2023-07-19 23:47:00 -0300

    Quiz aniversario de 22 anos.
    Quiz aniversario de 22 anos.

---
+++ questions.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    question: "Qual a palavra chave do primeiro papel?<br> Dica: O que você mais ama nesse ambiente?",
+    answers: [
+      { option: "MA", correct: false },
+      { option: "VE", correct: true },
+      { option: "HG", correct: false },
+    ],
+  },
+  {
+    question: "Qual a palavra chave do segundo papel?<br> Dica: Pessoa que mais usa 'Dudu' 'Gabi'?",
+    answers: [
+      { option: "J.", correct: false },
+      { option: "XO", correct: false },
+      { option: "M", correct: true },
+    ],
+  },
+  {
+    question: "Qual a palavra chave do terceiro papel?<br> Dica: Meu refúgio após me estressar no UNO?",
+    answers: [
+      { option: "U, ", correct: true },
+      { option: "JM", correct: false },
+      { option: "7C", correct: false },
+    ],
+  },
+  {
+    question: "Qual a palavra chave do quarto papel?<br> Dica: Onde você coloca filme e eu sempre durmo. ",
+    answers: [
+      { option: "RT", correct: false },
+      { option: "5F", correct: false },
+      { option: "GI", correct: true },
+    ],
+  },
+  {
+    question: "Qual a palavra chave do quinto papel?<br> Dica: Qual o item que eu mais dou prejuizo na sua casa?",
+    answers: [
+      { option: "8D", correct: false },
+      { option: "CV", correct: false },
+      { option: "Y", correct: true },
+    ],
+  },
+  {
+    question: "Qual a palavra chave do sexto papel?<br> Qual o coroa mais simpatico de ipiaú e região?",
+    answers: [
+      { option: "BM", correct: false },
+      { option: "LO", correct: true },
+      { option: "H7", correct: false },
+    ],
+  },
+  {
+    question: "ACERTOU TODAS! A ultima palavra chave é a junção de todas as palavras chaves em ordem + duas letras. Não se preocuope, assim que unir corretamente as que ja tem, ficará facil. Assim que formar, ganhará a dica final que lhe levará ao seu outro presente. OBS: A frase está em ingles.",
+   
+  }
+];
